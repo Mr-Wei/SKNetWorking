8e288d0f88566448da09142c9e2d7087cfaa4bbe
Wei <yuchen@17shihui.com>
2017-06-27 10:41:54 +0800

    init

---
+++ SKNetWorking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKNetWorking
+//
+//  Created by Wei on 2017/6/27.
+//  Copyright © 2017年 Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SKNetWorking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SKNetWorking
+//
+//  Created by Wei on 2017/6/27.
+//  Copyright © 2017年 Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
